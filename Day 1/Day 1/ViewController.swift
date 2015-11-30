9df8422266a4e4d68ed2eaf4bac75ab8c0c876bc
Harry Ng <harryworld@gmail.com>
2015-11-30 23:04:45 +0800

    Initial Commit.

---
+++ Day 1/Day 1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Day 1
+//
+//  Created by Harry Ng on 30/11/2015.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
