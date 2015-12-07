8aca6ecd2bc9da684ed317ddfa17c7ef97c2ab5a
Harry Ng <harryworld@gmail.com>
2015-12-07 22:00:49 +0800

    Add Array Controller

---
+++ ArrayController/ArrayController/Song.swift
@@ -0,0 +1,16 @@
+//
+//  Song.swift
+//  ArrayController
+//
+//  Created by Harry Ng on 7/12/2015.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class Song: NSObject {
+    
+    var name: String? = "New Song"
+    var bpm: Int = 100
+
+}
