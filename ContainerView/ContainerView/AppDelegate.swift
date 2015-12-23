8fdd89cd45b7aa405a5968fe77e36d815f54e188
Harry Ng <harryworld@gmail.com>
2015-12-23 23:30:34 +0800

    Add ContainerView

---
+++ ContainerView/ContainerView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ContainerView
+//
+//  Created by Harry Ng on 23/12/2015.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
