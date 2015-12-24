54b1e61aa83e5e067ac154ba3e09eb6a54bd6f42
Harry Ng <harryworld@gmail.com>
2015-12-24 21:49:21 +0800

    Add Lifecycle methods to NSViewController

---
+++ ContainerView/ContainerView/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  ContainerView
+//
+//  Created by Harry Ng on 24/12/2015.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        print("Main")
+    }
+    
+}
---
+++ ContainerView/ContainerView/SidebarViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SidebarViewController.swift
+//  ContainerView
+//
+//  Created by Harry Ng on 24/12/2015.
+//  Copyright © 2015 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class SidebarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        print("viewDidLoad")
+        print("view: \(view.window)")
+    }
+    
+    override func viewWillAppear() {
+        print("viewWillAppear")
+        print("view: \(view.window?.title)")
+        view.window?.title = "ContainerView"
+    }
+    
+    override func viewDidAppear() {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillLayout() {
+        print("viewWillLayout")
+    }
+    
+    override func viewDidLayout() {
+        print("viewDidLayout")
+    }
+    
+    override func viewWillDisappear() {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear() {
+        print("viewDidDisappear")
+    }
+    
+}
