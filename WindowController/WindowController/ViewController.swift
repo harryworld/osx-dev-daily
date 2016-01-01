ae273234764f0935280c8f7d3c428052f561f7ac
Harry Ng <harryworld@gmail.com>
2016-01-01 23:02:36 +0800

    Add WindowController

---
+++ WindowController/WindowController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindowController
+//
+//  Created by Harry Ng on 1/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
