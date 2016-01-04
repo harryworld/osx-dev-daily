87b4529d6e9a71b299ae978b9088b5c147db9765
Harry Ng <harryworld@gmail.com>
2016-01-04 23:59:28 +0800

    Modify Toolbar

---
+++ WindowController/WindowController/WindowController.swift
@@ -0,0 +1,20 @@
+//
+//  WindowController.swift
+//  WindowController
+//
+//  Created by Harry Ng on 4/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.titleVisibility = .Hidden
+    }
+    
+}
