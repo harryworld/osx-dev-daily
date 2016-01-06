3004a1abaee7e8c62bfa0032bddd4fcabd9d74e6
Harry Ng <harryworld@gmail.com>
2016-01-06 22:11:55 +0800

    Create new Project to demonstrate Binding

---
+++ Binding/Binding/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Binding
+//
+//  Created by Harry Ng on 6/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
