6286f73a6fbc4517010a8820fcc5653f504c59d4
Harry Ng <harryworld@gmail.com>
2016-01-10 21:29:25 +0800

    Create project for NSCollectionView since WWDC 2015

---
+++ NSCollectionView2015/NSCollectionView2015Tests/NSCollectionView2015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  NSCollectionView2015Tests.swift
+//  NSCollectionView2015Tests
+//
+//  Created by Harry Ng on 10/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import XCTest
+@testable import NSCollectionView2015
+
+class NSCollectionView2015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
