804f720f5dcab11d6fe39588a8aecf6e06b13f4f
Harry Ng <harryworld@gmail.com>
2016-01-22 20:31:19 +0800

    Create project for Event Handling

---
+++ EventHandling/EventHandling/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  EventHandling
+//
+//  Created by Harry Ng on 22/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var statusLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
