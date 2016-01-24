c7fe0c126b566a84801f8c4e00b69faaa4f42b41
Harry Ng <harryworld@gmail.com>
2016-01-24 19:08:32 +0800

    Add Gesture Recognizer

--- EventHandling/EventHandling/ViewController.swift
+++ EventHandling/EventHandling/ViewController.swift
@@ -8,19 +8,52 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSGestureRecognizerDelegate {
 
     @IBOutlet weak var statusLabel: NSTextField!
+    var click: NSClickGestureRecognizer?
+    var doubleClick: NSClickGestureRecognizer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let click = NSClickGestureRecognizer(target: self, action: "click:")
+        click.delegate = self
+        view.addGestureRecognizer(click)
+        self.click = click
+        
+        let doubleClick = NSClickGestureRecognizer(target: self, action: "doubleClick:")
+        doubleClick.numberOfClicksRequired = 2
+        view.addGestureRecognizer(doubleClick)
+        self.doubleClick = doubleClick
     }
     
     override func viewDidAppear() {
         view.window?.makeFirstResponder(self)
     }
+    
+    // MARK: Gesture Recognizers
+    
+    func click(gesture: NSClickGestureRecognizer) {
+        print("click")
+        statusLabel.stringValue = "Click"
+    }
+    
+    func doubleClick(gesture: NSClickGestureRecognizer) {
+        print("doubleClick")
+        statusLabel.stringValue = "doubleClick"
+    }
+    
+    func gestureRecognizer(gestureRecognizer: NSGestureRecognizer, shouldRequireFailureOfGestureRecognizer otherGestureRecognizer: NSGestureRecognizer) -> Bool {
+        if gestureRecognizer == click && otherGestureRecognizer == doubleClick {
+            return true
+        }
+        return false
+    }
+    
+    // MARK: Key Events
     
     override func keyDown(theEvent: NSEvent) {
         statusLabel.stringValue = "No Specific Event"
@@ -33,6 +66,8 @@ class ViewController: NSViewController {
             statusLabel.stringValue = "keyDown: Space"
         }
     }
+    
+    // MARK: Mouse Events
     
     override func mouseDown(theEvent: NSEvent) {
         print("mouseDown")
