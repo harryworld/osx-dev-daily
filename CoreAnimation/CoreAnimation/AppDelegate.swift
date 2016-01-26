478f7d6c93bf2c3d81a7fc7306e67483021f5b47
Harry Ng <harryworld@gmail.com>
2016-01-26 21:59:36 +0800

    Create Core Animation Project

---
+++ CoreAnimation/CoreAnimation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoreAnimation
+//
+//  Created by Harry Ng on 26/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoreAnimation/CoreAnimationTests/CoreAnimationTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreAnimationTests.swift
+//  CoreAnimationTests
+//
+//  Created by Harry Ng on 26/1/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import XCTest
+@testable import CoreAnimation
+
+class CoreAnimationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
