ac8b68f2583e8ce4eff5c2bd59b9e0e2255055aa
Harry Ng <harryworld@gmail.com>
2016-01-28 19:56:27 +0800

    Add Attributed Text to text field

--- NSFont/NSFont/ViewController.swift
+++ NSFont/NSFont/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let font = NSFont.systemFontOfSize(22)
+        print("\(font.capHeight)")
+        
+        let textField = NSTextField(frame: NSRect(x: 100, y: 100, width: 200, height: ceil(font.capHeight) + 20))
+        textField.font = font
+        textField.editable = false
+        textField.alignment = .Center
+        textField.stringValue = "OSX Dev Daily"
+        view.addSubview(textField)
+        
+        let attrStr = NSMutableAttributedString(string: "OSX Dev Daily")
+        attrStr.addAttribute(NSUnderlineStyleAttributeName, value: 1, range: NSRange(8...12))
+        attrStr.addAttribute(NSFontAttributeName, value: NSFont.boldSystemFontOfSize(22), range: NSRange(0...2))
+        attrStr.addAttribute(NSForegroundColorAttributeName, value: NSColor.redColor(), range: NSRange(0...2))
+        textField.attributedStringValue = attrStr
     }
 
     override var representedObject: AnyObject? {
