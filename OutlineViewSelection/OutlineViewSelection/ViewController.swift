4b126871f03fd042b980fbfe8c11656f7547ff1a
Harry Ng <harryworld@gmail.com>
2016-02-21 21:50:16 +0800

    Add NSOutlineView and basic delegate/datasource implementation

--- OutlineViewSelection/OutlineViewSelection/ViewController.swift
+++ OutlineViewSelection/OutlineViewSelection/ViewController.swift
@@ -10,18 +10,82 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var strings = ["A", "B"]
+    
+    @IBOutlet weak var outlineView: NSOutlineView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        outlineView.setDelegate(self)
+        outlineView.setDataSource(self)
+        
+        outlineView.expandItem(nil, expandChildren: true)
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    
+    func isHeader(item: AnyObject) -> Bool {
+        return (item as? String) == "Labels"
     }
-
 
 }
 
+extension ViewController {
+    
+}
+
+extension ViewController: NSOutlineViewDataSource {
+    
+    func outlineView(outlineView: NSOutlineView, numberOfChildrenOfItem item: AnyObject?) -> Int {
+        if item == nil {
+            return 1
+        } else {
+            return strings.count
+        }
+    }
+    
+    func outlineView(outlineView: NSOutlineView, isItemExpandable item: AnyObject) -> Bool {
+        return isHeader(item)
+    }
+    
+    func outlineView(outlineView: NSOutlineView, child index: Int, ofItem item: AnyObject?) -> AnyObject {
+        if item == nil {
+            return "Labels"
+        } else {
+            return strings[index]
+        }
+    }
+    
+    func outlineView(outlineView: NSOutlineView, objectValueForTableColumn tableColumn: NSTableColumn?, byItem item: AnyObject?) -> AnyObject? {
+        return item
+    }
+    
+}
+
+extension ViewController: NSOutlineViewDelegate {
+    
+    func outlineView(outlineView: NSOutlineView, viewForTableColumn tableColumn: NSTableColumn?, item: AnyObject) -> NSView? {
+        
+        if isHeader(item) {
+            return outlineView.makeViewWithIdentifier("HeaderCell", owner: self)
+        } else {
+            let view = outlineView.makeViewWithIdentifier("DataCell", owner: self) as? NSTableCellView
+            
+            if let item = item as? String {
+                view?.textField?.stringValue = item
+            }
+            
+            return view
+        }
+    }
+    
+    func outlineView(outlineView: NSOutlineView, shouldShowOutlineCellForItem item: AnyObject) -> Bool {
+        return false
+    }
+    
+    func outlineView(outlineView: NSOutlineView, shouldSelectItem item: AnyObject) -> Bool {
+        return !isHeader(item)
+    }
+    
+}
