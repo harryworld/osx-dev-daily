76b8fcfb8dbc8abf636934186ac4e3712c3d5579
Harry Ng <harryworld@gmail.com>
2016-02-23 22:53:14 +0800

    Setup Pomodoro Timer

--- NSTimer/NSTimer/ViewController.swift
+++ NSTimer/NSTimer/ViewController.swift
@@ -10,18 +10,42 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var timeLabel: NSTextField!
+    
+    let countDown: Double = 25 * 60
+    var currentSecond: Double = 0
+    var timer: NSTimer? {
+        didSet {
+            oldValue?.invalidate()
+        }
+    }
+    
+    lazy var formatter: NSDateComponentsFormatter = {
+        let formatter = NSDateComponentsFormatter()
+        formatter.allowedUnits = [.Minute, .Second]
+        formatter.zeroFormattingBehavior = .Pad
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    @IBAction func start(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "update", userInfo: nil, repeats: true)
+        currentSecond = countDown
     }
-
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer = nil
+    }
+    
+    func update() {
+        timeLabel.stringValue = "\(formatter.stringFromTimeInterval(currentSecond)!)"
+        currentSecond--
+    }
 
 }
 
