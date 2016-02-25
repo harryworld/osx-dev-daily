4f32bafec59361d83b479b74bb69a712de3afc7b
Harry Ng <harryworld@gmail.com>
2016-02-25 23:19:41 +0800

    Create Multiple Windows Project

---
+++ MultipleWindows/MultipleWindows/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MultipleWindows
+//
+//  Created by Harry Ng on 25/2/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MultipleWindows/MultipleWindows/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MultipleWindows
+//
+//  Created by Harry Ng on 25/2/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
