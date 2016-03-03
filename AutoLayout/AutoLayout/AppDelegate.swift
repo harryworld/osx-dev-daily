b1e201eb2df6b7e259e92f40d82ab8073b26770b
Harry Ng <harryworld@gmail.com>
2016-03-03 22:28:27 +0800

    Create AutoLayout Project

---
+++ AutoLayout/AutoLayout/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutoLayout
+//
+//  Created by Harry Ng on 3/3/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
