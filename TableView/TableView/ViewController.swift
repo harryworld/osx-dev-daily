766b3312c75375a21e85990fd23fc598a1d1aa8d
Harry Ng <harryworld@gmail.com>
2016-03-05 21:54:53 +0800

    Search NSTableView without ArrayController

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -11,6 +11,18 @@ import Cocoa
 class ViewController: NSViewController, NSTableViewDataSource, NSTextFieldDelegate {
     
     var strings = ["apple", "banana", "orange", "watermelon"]
+    var backupStrings: [String]!
+    var isSearching = false {
+        didSet {
+            if isSearching != oldValue {
+                if isSearching {
+                    backupStrings = strings
+                } else {
+                    strings = backupStrings
+                }
+            }
+        }
+    }
 
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var tableView: NSTableView!
@@ -31,6 +43,21 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTextFieldDelega
         let string = textField.stringValue
         strings.append(string)
         textField.stringValue = ""
+        tableView.reloadData()
+    }
+    
+    @IBAction func searchWith(sender: NSSearchField) {
+        if sender.stringValue.isEmpty {
+            print("isEmpty")
+            isSearching = false
+        } else {
+            print(sender.stringValue)
+            isSearching = true
+            
+            strings = backupStrings.filter { s in
+                return s.lowercaseString.containsString(sender.stringValue.lowercaseString)
+            }
+        }
         tableView.reloadData()
     }
     
