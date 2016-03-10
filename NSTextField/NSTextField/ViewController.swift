326f2459b7e6c7c73f6eb02df02e1dc4e2e0a407
Harry Ng <harryworld@gmail.com>
2016-03-10 20:29:08 +0800

    Create project for NSTextField

---
+++ NSTextField/NSTextField/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTextField
+//
+//  Created by Harry Ng on 10/3/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
