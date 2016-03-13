12e4e6296548f07b57945c90b093a14b87a8e13d
Harry Ng <harryworld@gmail.com>
2016-03-13 22:04:58 +0800

    Add NSPopUpButton

--- NSControl/NSControl/ViewController.swift
+++ NSControl/NSControl/ViewController.swift
@@ -10,10 +10,23 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var genderControl: NSPopUpButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // NSTextField
+        textField.stringValue = "Harry"
+        
+        // Gender
+        genderControl.removeAllItems()
+        genderControl.addItemsWithTitles(["Male", "Female"])
+        let selectionIndex = genderControl.indexOfSelectedItem
+        print("selected gender index: \(selectionIndex)")
+        genderControl.selectItemAtIndex(1)
     }
 
     override var representedObject: AnyObject? {
