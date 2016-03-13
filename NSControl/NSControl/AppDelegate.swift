d0f64c85900781823f9e7dba17c45ab909de572e
Harry Ng <harryworld@gmail.com>
2016-03-13 21:47:29 +0800

    Create Project for NSControl

---
+++ NSControl/NSControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSControl
+//
+//  Created by Harry Ng on 13/3/2016.
+//  Copyright © 2016 STAY REAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
